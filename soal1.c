6ef1c5bdd8000034a016ec010ad650e0da426bf6
nadyaaafff <moaningcilka7@gmail.com>
2026-04-13 09:30:47 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+int main(){
+    float suhu;
+    int izin, radiasi, jam;
+    printf("Masukkan izin:");
+    scanf("%d", &izin);
+    printf("Masukkan suhu (dalam 0.1 derajat celcius):");
+    scanf("%f", &suhu);
+    printf("Masukkan radiasi:");
+    scanf("%d", &radiasi);
+    printf("Masukkan jam:");
+    scanf("%d", &jam);
+    if (radiasi >= 6){
+        printf("TOLAK");
+    } else if (suhu >= 390){
+        printf("KARANTINA");
+    } else if (izin == 1 && jam < 6 || jam > 20){
+        printf("TOLAK");
+    } else if (izin == 1){
+        printf("MASUK");
+    }else if(izin == 2 && radiasi <= 2 && 8 < jam < 18){
+        printf("MASUK");
+    } else if(izin == 2){
+        printf("PEMERIKSAAN");
+    } else if (izin == 3 && radiasi < 380){
+        printf("MASUK");
+    }else{
+        printf("TOLAK");
+    }
+    return 0;}
 
-int main(void) {
-    return 0;
-}
+
+
+
