a208fb843e0b4eaa48ec81e2832aefefbc81164b
nadyaaafff <moaningcilka7@gmail.com>
2026-04-13 10:33:03 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -4,24 +4,29 @@ int main(){
     scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
     if (radiasi >= 6){
         printf("TOLAK");
-    } else if (suhu >= 390){
-        printf("KARANTINA");
-    } else if (izin == 1){
-        printf("MASUK");
-        if(jam < 6 || jam > 20){
-            printf("TOLAK");
-        }
-    } else if (izin == 2){
-        printf("PEMERIKSAAN");
-        if(radiasi <= 2 && jam > 8 && jam < 18){
-            printf("TOLAK");
-        }
-    } else if (izin == 3){
-        printf("MASUK");
-        if(radiasi == 0 && suhu < 380){
-            printf("TOLAK");
-        }
     }
+    else if (suhu >= 390){
+        printf("KARANTINA");
+    }
+
+    else if (izin == 1 && radiasi != 0 && radiasi < 6 && radiasi > 2){
+        printf("MASUK");
+
+    }else if(jam < 6 || jam > 20){
+            printf("TOLAK");
+        }
+    else if (izin == 2 && suhu < 390 && suhu >= 380 && radiasi != 0 && radiasi <= 2){
+        printf("PEMERIKSAAN");}
+        else if(radiasi <= 2 && jam >= 8 && jam <= 18){
+            printf("MASUK");
+        }
+    
+    else if (izin == 3 && radiasi != 0 &&suhu < 380 && suhu >= 370){
+        printf("MASUK");}
+        else if(radiasi == 0 && suhu < 380){
+            printf("TOLAK");
+        }
+    
     else{
         printf("TOLAK");
     }
