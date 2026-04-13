43dd61cdf708d9b59ece5d402d901ecb3fa1a729
nadyaaafff <moaningcilka7@gmail.com>
2026-04-13 09:42:52 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 int main(){
     float suhu;
-    int izin, radiasi, jam;
-    printf("Masukkan izin:");
-    scanf("%d", &izin);
-    printf("Masukkan suhu (dalam 0.1 derajat celcius):");
-    scanf("%f", &suhu);
-    printf("Masukkan radiasi:");
-    scanf("%d", &radiasi);
-    printf("Masukkan jam:");
-    scanf("%d", &jam);
+    int izin, radiasi, jam; 
+    scanf("%d %d %d %d", &izin, &suhu, &radiasi, &jam);
     if (radiasi >= 6){
         printf("TOLAK");
     } else if (suhu >= 390){
@@ -34,3 +27,6 @@ int main(){
         printf("TOLAK");
     }
     return 0;}
+
+
+
