58907391aa8e0f9f7023f837c18159d399dad875
nadyaaafff <moaningcilka7@gmail.com>
2026-04-13 09:40:58 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -14,17 +14,23 @@ int main(){
         printf("TOLAK");
     } else if (suhu >= 390){
         printf("KARANTINA");
-    } else if (izin == 1 && jam < 6 || jam > 20){
-        printf("TOLAK");
     } else if (izin == 1){
         printf("MASUK");
-    }else if(izin == 2 && radiasi <= 2 && 8 < jam < 18){
-        printf("MASUK");
-    } else if(izin == 2){
+        if(jam >= 6 && jam <= 20){
+            printf("TOLAK");
+        }
+    } else if (izin == 2){
         printf("PEMERIKSAAN");
-    } else if (izin == 3 && radiasi < 380){
+        if(radiasi > 2 || jam <= 8 || jam >= 18){
+            printf("TOLAK");
+        }
+    } else if (izin == 3){
         printf("MASUK");
-    }else{
+        if(radiasi >= 380){
+            printf("TOLAK");
+        }
+    }
+    else{
         printf("TOLAK");
     }
     return 0;}
